03922abdf1c0b57110e7665e25bc3ce0884f91de
Kristen Wilde <kristen.wilde@gmail.com>
2018-08-27 13:54:24 -0400

    update styles

--- projects.js
+++ projects.js
@@ -1,22 +1,43 @@
 module.exports = [
   {
+    title: 'MyNotePractice',
+    img: 'note_quiz_wide.png',
+    img_url: '/images/note_quiz.png',
+    description:
+      'This music teaching aid guides \
+        users through setting goals, selecting a goal, completing a \
+        practice session, and viewing results. A collaboration with Kingsley Victor (node API server) and Miguel Rivera.',
+    tech: 'MERN stack',
+    links: [{ name: 'Github', url: 'https://github.com/KristenWilde/note-practice-app-1' }],
+  },
+  {
     title: 'Subtraction App',
     img: 'subtraction_app.png',
     img_url: 'http://subtract.online',
-    description: "A game-like educational web app designed entirely by me. Tracks each user's \
+    description:
+      "A game-like educational web app designed entirely by me. Tracks each user's \
         progress toward mastering basic arithmetic, \
         as well as time spent and problems practiced per day.",
-    more: "Vue modules respond to data, user interaction, and optional query parameters. Designed to protect children's privacy.",
+    more:
+      "Vue modules respond to data, user interaction, and optional query parameters. Designed to protect children's privacy.",
     tech: 'Ruby on Rails, Vue.js, HTML, SCSS',
     links: [
-      { name: 'Live site',
-        url: 'http://subtract.online'},
-      { name: 'Github: sample Rails controller',
-        url: 'https://github.com/KristenWilde/First-grade-math/blob/master/app/controllers/users_controller.rb'},
-      { name: 'Github: sample Rails model',
-        url: 'https://github.com/KristenWilde/First-grade-math/blob/master/app/models/user.rb' },
-      { name: 'Github: Vue modules',
-        url: 'https://github.com/KristenWilde/First-grade-math/tree/master/app/javascript'}
+      {
+        name: 'Live site',
+        url: 'http://subtract.online',
+      },
+      {
+        name: 'Github: sample Rails controller',
+        url: 'https://github.com/KristenWilde/First-grade-math/blob/master/app/controllers/users_controller.rb',
+      },
+      {
+        name: 'Github: sample Rails model',
+        url: 'https://github.com/KristenWilde/First-grade-math/blob/master/app/models/user.rb',
+      },
+      {
+        name: 'Github: Vue modules',
+        url: 'https://github.com/KristenWilde/First-grade-math/tree/master/app/javascript',
+      },
     ],
   },
   {
@@ -26,49 +47,64 @@ module.exports = [
     description: 'Allows user to input, edit, and delete contacts. Filter by tag or search by name.',
     tech: 'JavaScript, jQuery, Handlebars, HTML, SCSS',
     links: [
-      { name: 'Github',
-        url: 'https://github.com/KristenWilde/contact_manager/blob/master/public/javascripts/contacts.js'},
-    ]
+      {
+        name: 'Github',
+        url: 'https://github.com/KristenWilde/contact_manager/blob/master/public/javascripts/contacts.js',
+      },
+    ],
   },
   {
     title: 'Todo App 1',
     img: 'todo_manager.png',
     img_url: '/images/todo_manager.png',
-    description: 'Allows user to manage items, displays lists automatically \
+    description:
+      'Allows user to manage items, and displays lists automatically \
       sorted by due date and completion. Pixel-perfect responsive HTML/CSS from Photoshop files.',
-    tech: "JavaScript, jQuery, Handlebars, HTML, CSS",
+    tech: 'JavaScript, jQuery, Handlebars, HTML, CSS',
     links: [
-      { name: 'Github',
-        url: 'https://github.com/KristenWilde/Todo_Manager/blob/master/public/javascripts/todos.js' },
-    ]
+      {
+        name: 'Github',
+        url: 'https://github.com/KristenWilde/Todo_Manager/blob/master/public/javascripts/todos.js',
+      },
+    ],
   },
-  { title: 'Todo App 2',
+  {
+    title: 'Todo App 2',
     img: 'todo_tracker.png',
     img_url: 'https://glacial-woodland-26684.herokuapp.com/',
     description: 'Another todo app. CRUD actions for items and lists.',
     tech: 'Sinatra, SQL',
     more: '',
     links: [
-      { name: 'Live demo',
-        url: 'https://glacial-woodland-26684.herokuapp.com/' },
-      { name: 'Github',
-        url: 'https://github.com/KristenWilde/Todo-tracker'},
+      {
+        name: 'Live demo',
+        url: 'https://glacial-woodland-26684.herokuapp.com/',
+      },
+      {
+        name: 'Github',
+        url: 'https://github.com/KristenWilde/Todo-tracker',
+      },
     ],
   },
   {
     title: 'Sample App',
     img: 'sample_app.png',
     img_url: 'https://sample-app-135.herokuapp.com/',
-    description: "If I finish this tutorial project, the world will have another Twitter clone. \
-        As is, you can sign up and view four pages of seed users. Developed with a full suite of tests.",
+    description:
+      'If I finish this tutorial project, the world will have another Twitter clone. \
+        As is, you can sign up and view four pages of seed users. Developed with a full suite of tests.',
     tech: 'Ruby on Rails, TDD with Minitest',
     more: '',
     links: [
-      { name: 'Live demo',
-        url: 'https://sample-app-135.herokuapp.com/' },
-      { name: 'Github',
-        url: 'https://github.com/KristenWilde/sample-app' }
-    ]
+      {
+        name: 'Live demo',
+        url: 'https://sample-app-135.herokuapp.com/',
+      },
+      {
+        name: 'Github',
+        url: 'https://github.com/KristenWilde/sample-app',
+      },
+    ],
   },
   {
     title: 'Guess-a-word',
@@ -77,11 +113,15 @@ module.exports = [
     description: 'Guess the word before all the apples fall from the tree. Not for mobile.',
     tech: 'JavaScript',
     links: [
-      { name: 'Live demo',
-        url: '/guess'},
-      { name: 'Github',
-        url: 'https://github.com/KristenWilde/Launch-School-Javascript/tree/master/220/guess_a_word'}
-    ]
+      {
+        name: 'Live demo',
+        url: '/guess',
+      },
+      {
+        name: 'Github',
+        url: 'https://github.com/KristenWilde/Launch-School-Javascript/tree/master/220/guess_a_word',
+      },
+    ],
   },
   {
     title: 'Book Viewer',
@@ -91,11 +131,15 @@ module.exports = [
     tech: 'Ruby, Sinatra',
     more: '',
     links: [
-      { name: 'Live demo',
-        url: 'https://ls-book-viewer-170.herokuapp.com/search' },
-      { name: 'Github',
-        url: 'https://github.com/KristenWilde/Launch-School-Ruby/tree/master/sinatra/book_viewer' }
-    ]
+      {
+        name: 'Live demo',
+        url: 'https://ls-book-viewer-170.herokuapp.com/search',
+      },
+      {
+        name: 'Github',
+        url: 'https://github.com/KristenWilde/Launch-School-Ruby/tree/master/sinatra/book_viewer',
+      },
+    ],
   },
   {
     title: 'Command line apps',
@@ -105,13 +149,18 @@ module.exports = [
     more: '',
     tech: 'Ruby, Rubocop, SQL',
     links: [
-      { name: 'Github: Tic-tac-toe and 21',
-        url: 'https://github.com/KristenWilde/Launch-School-Ruby/blob/master/object_oriented_ruby/lesson_5/otherttt.rb'},
-      { name: 'Github: Rock paper scissors lizard spock',
-        url: 'https://github.com/KristenWilde/Launch-School-Ruby/blob/master/object_oriented_ruby/lesson_2/rpsls.rb'},
-      { name: 'Github: Recipe manager',
-        url: 'https://github.com/KristenWilde/dishlist'}
-    ]
+      {
+        name: 'Github: Tic-tac-toe and 21',
+        url: 'https://github.com/KristenWilde/Launch-School-Ruby/blob/master/object_oriented_ruby/lesson_5/otherttt.rb',
+      },
+      {
+        name: 'Github: Rock paper scissors lizard spock',
+        url: 'https://github.com/KristenWilde/Launch-School-Ruby/blob/master/object_oriented_ruby/lesson_2/rpsls.rb',
+      },
+      {
+        name: 'Github: Recipe manager',
+        url: 'https://github.com/KristenWilde/dishlist',
+      },
+    ],
   },
 ]
-
