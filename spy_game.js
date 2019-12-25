64589c2ff95645d31de47983098e10f8491177a7
Kristen Wilde <kristen.wilde@gmail.com>
2019-12-25 06:25:55 -0800

    tries to use onclick attribute

--- spy_game.js
+++ spy_game.js
@@ -3,9 +3,18 @@ module.exports = {
   players: 4,
   spies: 1,
   locations: "Hawaii\nPrison\nFletcher's house",
-  startGame: function(){
+  status: "setup",
+  startGame: function(e){
+    alert('starting game')
     this.spy_index = Math.floor(Math.random() * (this.players))
     this.selected_location = locations.split()[spy_index]
+    this.started = true
   },
-
+  showPlayer: function(e){
+    let span = e.target
+    alert('called')
+  },
+  hidePlayer: function(player_index){
+    let span = document.getElementById(`player-${player_index}-hide`)
+  },
 }
